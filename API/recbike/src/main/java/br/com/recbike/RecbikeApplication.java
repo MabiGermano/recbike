ea5c82b42f8aa2abddb86fba2711fdaefd3cdbd8
ThullyoCunha <thullyo.ighor@gmail.com>
2022-11-19 16:02:11 -0300

    add start api

---
+++ API/recbike/src/main/java/br/com/recbike/RecbikeApplication.java
@@ -0,0 +1,13 @@
+package br.com.recbike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecbikeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecbikeApplication.class, args);
+	}
+
+}
---
+++ API/recbike/src/test/java/br/com/recbike/RecbikeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.recbike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecbikeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
